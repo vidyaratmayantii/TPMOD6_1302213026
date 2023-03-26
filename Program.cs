968a9fa24038839b7a12ff139ca3d7d669d56926
LAPTOP-GTQCMDG9\Asus <vidyaratmayanti@gmail.com>
2023-03-26 17:26:34 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TPMOD6_1302213026;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [VIDYA ANANDA R]");
+        video.IncreasePlayCount(1000000000);
+        video.IncreasePlayCount(1000000000);
+        video.printVideoDetail();
+
+
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD6_1302213026
+{
+    public class SayaTubeVideo
+    {
+        int id;
+        string title;
+        int playCount;
+
+        public SayaTubeVideo(string judul_video) {
+            Random rnd = new Random();
+            id = rnd.Next();
+            this.title = judul_video;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount) {
+           this.playCount += playCount;  
+             
+        }
+
+        public void printVideoDetail() {
+            Console.WriteLine("Judul video adalah " + title + " dengan id " + id + " diputar sebanyak " + playCount + " kali");
+        }
+    }
+
+
+}
